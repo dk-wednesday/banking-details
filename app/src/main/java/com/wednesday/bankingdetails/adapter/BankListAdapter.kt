c48cdfbc69a1220bb8a8dc452b914ea7dfbc77d4
Dhananjay Kulkarni <dhananjay@wednesday.is>
2021-04-07 13:02:24 +0530

    Using Kotlin extensions
    Used Kotlin extensions to avoid use of findViewByID

--- app/src/main/java/com/wednesday/bankingdetails/adapter/BankListAdapter.kt
+++ app/src/main/java/com/wednesday/bankingdetails/adapter/BankListAdapter.kt
@@ -3,24 +3,33 @@ package com.wednesday.bankingdetails.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.wednesday.bankingdetails.R
 import com.wednesday.bankingdetails.model.Bank
 import com.wednesday.bankingdetails.utility.Utils
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.row_item_bank.view.*
 import java.util.*
 
 class BankListAdapter(var bankDataSource: ArrayList<Bank>) :
-        RecyclerView.Adapter<BankListAdapter.ViewHolder>() {
+    RecyclerView.Adapter<BankListAdapter.ViewHolder>() {
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val txtBankName: TextView = itemView.findViewById(R.id.txtBankName)
-        val txtIfsCode: TextView = itemView.findViewById(R.id.txtIfsCode)
+    class ViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
+        LayoutContainer {
+
+        fun bind(bank: Bank) {
+            val name = "${itemView.resources.getString(R.string.bank_name)}: ${bank.bankName}"
+            val ifsc = "${itemView.resources.getString(R.string.ifsc)}: ${bank.ifsCode}"
+            itemView.txtBankName.text = Utils.capitalizeFirstLetter(name)
+            itemView.txtIfsCode.text = ifsc.toUpperCase(
+                Locale.getDefault()
+            )
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view =
-                LayoutInflater.from(parent.context).inflate(R.layout.row_item_bank, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.row_item_bank, parent, false)
         return ViewHolder(view)
     }
 
@@ -30,13 +39,7 @@ class BankListAdapter(var bankDataSource: ArrayList<Bank>) :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val bank = bankDataSource[position]
-
-        val name = "${holder.itemView.resources.getString(R.string.bank_name)}: ${bank.bankName}"
-        val ifsc = "${holder.itemView.resources.getString(R.string.ifsc)}: ${bank.ifsCode}"
-        holder.txtBankName.text = Utils.capitalizeFirstLetter(name)
-        holder.txtIfsCode.text = ifsc.toUpperCase(
-                Locale.getDefault()
-        )
+        holder.bind(bank)
     }
 
     fun resetDataSource(bank: List<Bank>) {
